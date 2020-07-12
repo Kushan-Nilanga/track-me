6bfca1753565aabdc31ddcc3ffbb02624f6a3b64
Kushan-Nilanga <dknathalage@gmail.com>
2020-07-12 17:08:30 +1000

    ENHANCE: Add register-device.html and functionality to add a device

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const devices = [];
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
+/* Depricated - Barebone JS
 devices.forEach(function(device) {
     const table = document.querySelector('#devices');
     const row = document.createElement('tr');
@@ -20,4 +18,32 @@ devices.forEach(function(device) {
 
     table.appendChild(row);
 
-})
+}) */
+
+/* viewing devices details using JQuery */
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`);
+});
+
+/* Depricated - Barebone JS
+document.querySelector('#add-device').addEventListener('click',
+    function() {
+        const user = document.querySelector('#user').value;
+        const name = document.querySelector('#name').value;
+        devices.push({ user: user, name: name });
+        console.log(devices);
+    });
+    */
+
+// ONCLICK event handler to add devices    
+$('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = 'device-list.html';
+});
